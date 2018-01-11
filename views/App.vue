571bb06058a62d81432a821bc21ad687a9afb762
jinboMa <596756194@qq.com>
2018-01-12 00:09:44 +0800

    add example & bunlde file

---
+++ views/App.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+.app
+  h1 {{ msg }}
+  my-test
+</template>
+
+<script>
+import MyTest from './MyTest.vue'
+export default {
+  components: { MyTest },
+  data () {
+    return {
+      msg: 'hello'
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ views/MyTest.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+.my-test
+  h2 {{ msg }}
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'hello this is test'
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+.my-test {
+  background-color: skyblue;
+}
+</style>
